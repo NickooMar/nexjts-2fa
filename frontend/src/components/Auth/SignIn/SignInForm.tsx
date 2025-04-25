2b309f19477bd00d98c94ff034a46696dc554eb7
NickooMar <nicoo.marsili@gmail.com>
2025-04-25 20:53:34 -0300

    fix(auth): improve styling and auth flow on singin

--- frontend/src/components/Auth/SignIn/SignInForm.tsx
+++ frontend/src/components/Auth/SignIn/SignInForm.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { Form } from "../../ui/form";
-import { EmailStep } from "./EmailStep";
-import { ArrowLeft } from "lucide-react";
+import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
 import { useToast } from "@/hooks/useToast";
-import { PasswordStep } from "./PasswordStep";
+import { Input } from "@/components/ui/input";
+import Spinner from "@/components/ui/spinner";
 import { Button } from "@/components/ui/button";
+import GoogleIcon from "@mui/icons-material/Google";
+import React, { useCallback, useState } from "react";
+import { Edit, Mail, MailCheck } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Separator } from "@/components/ui/separator";
+import { PasswordInput } from "../Inputs/PasswordInput";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { createSignInSchema } from "@/schemas/auth.schema";
-import React, { useCallback, useMemo, useState } from "react";
 import { DotBackground } from "../../Aceternity/DotBackground";
+import { Form, FormField, FormItem, FormMessage } from "../../ui/form";
 import { AuthProviders, SignInFormState } from "@/types/auth/auth.types";
 import { signInAction, signInWithProvider } from "@/app/actions/auth.actions";
 
@@ -21,6 +25,8 @@ const SignInForm: React.FC = () => {
   const { error: errorToast } = useToast();
   const signInSchema = createSignInSchema(t);
 
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [step, setStep] = useState<"email" | "password">("email");
 
   const form = useForm<SignInFormState>({
@@ -30,7 +36,12 @@ const SignInForm: React.FC = () => {
       password: "",
     },
   });
-  const { trigger, clearErrors } = form;
+  const { trigger, clearErrors, resetField } = form;
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   async function handleSigninProviders(provider: AuthProviders) {
     await signInWithProvider(provider);
@@ -40,8 +51,10 @@ const SignInForm: React.FC = () => {
     values: SignInFormState
   ) => {
     try {
-      const result = await signInAction(values);
-      console.log(result);
+      console.log({ values });
+
+      // const result = await signInAction(values);
+      // console.log(result);
     } catch (error) {
       console.error(error);
       errorToast(t("signin.messages.errors.invalid_data"));
@@ -49,45 +62,33 @@ const SignInForm: React.FC = () => {
   };
 
   const onNextStep = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    setIsLoading(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const validEmailStep = await trigger("email");
 
     // TODO: validate the email in the database
 
     if (validEmailStep) setStep("password");
+
+    setIsLoading(false);
   };
 
   const handleBackStep = useCallback(() => {
     clearErrors();
     setStep("email");
-  }, [step, clearErrors]);
+    resetField("password");
+  }, [clearErrors, resetField]);
 
-  const renderStep = useMemo(() => {
-    const mappedSteps = {
-      email: (
-        <EmailStep
-          form={form}
-          onNextStep={onNextStep}
-          onProviderSignin={handleSigninProviders}
-        />
-      ),
-      password: <PasswordStep form={form} />,
-    };
-    return mappedSteps[step];
-  }, [step]);
+  const handleAction = useCallback(async () => {
+    if (step === "email") await onNextStep();
+  }, [step, onNextStep]);
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <DotBackground>
         <section className="w-full max-w-md mx-auto p-4 sm:p-6 md:p-8 z-0 bg-card border border-border rounded-xl shadow relative">
-          {step === "password" && (
-            <div className="absolute top-2 left-2">
-              <Button variant="ghost" size="icon" onClick={handleBackStep}>
-                <ArrowLeft />
-              </Button>
-            </div>
-          )}
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="flex flex-col justify-center items-center space-y-2 gap-2">
@@ -98,7 +99,98 @@ const SignInForm: React.FC = () => {
                   {t("signin.description")}
                 </p>
               </div>
-              <section className="space-y-4">{renderStep}</section>
+              <section className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="relative">
+                        <Input
+                          {...field}
+                          type="email"
+                          required
+                          aria-label="email"
+                          placeholder={t("signin.email.placeholder")}
+                          disabled={step === "password"}
+                          className="pr-10"
+                          onChange={(e) => {
+                            field.onChange(e);
+                            setIsEmailValid(validateEmail(e.target.value));
+                          }}
+                        />
+                        {step === "password" ? (
+                          <Edit
+                            className="absolute inset-y-0 right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground hover:cursor-pointer"
+                            onClick={handleBackStep}
+                          />
+                        ) : isEmailValid ? (
+                          <MailCheck className="absolute inset-y-0 right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-green-500" />
+                        ) : (
+                          <Mail className="absolute inset-y-0 right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                        )}
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {step === "password" && (
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <PasswordInput
+                          {...field}
+                          id="password"
+                          required
+                          aria-label="password"
+                          placeholder={t("signin.password.title")}
+                          className="w-full"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+                <Button
+                  type={step === "password" ? "submit" : "button"}
+                  className="w-full rounded-xl mt-4 p-4"
+                  disabled={isLoading}
+                  onClick={handleAction}
+                >
+                  {isLoading ? <Spinner /> : t("signin.login")}
+                </Button>
+                <div className="flex justify-center items-center">
+                  <Separator className="w-[35%]" />
+                  <p className="text-sm text-muted-foreground mx-4 whitespace-nowrap">
+                    {t("signin.continue_with")}
+                  </p>
+                  <Separator className="w-[35%]" />
+                </div>
+                <div className="w-full grid grid-auto-fit-sm gap-4 place-content-center mt-5">
+                  <Button
+                    type="button"
+                    className="rounded-xl p-4"
+                    disabled={isLoading}
+                    onClick={async () =>
+                      await handleSigninProviders(AuthProviders.Google)
+                    }
+                  >
+                    <GoogleIcon className="mx-2" />
+                    {t("signin.google_auth")}
+                  </Button>
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                  <p className="text-sm">{t("signin.not_registered")}</p>
+                  <Link
+                    href="/signup"
+                    className="text-sm text-secondary-action underline"
+                  >
+                    {t("signin.create_account")}
+                  </Link>
+                </div>
+              </section>
             </form>
           </Form>
         </section>
